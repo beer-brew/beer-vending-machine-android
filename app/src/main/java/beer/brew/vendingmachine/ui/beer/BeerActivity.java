004ade53ce764cbfc9bd81b9e0d31e98f4f30e41
nancymi <13121178nancy@gmail.com>
2016-09-06 16:04:51 +0800

    Init: Beer MVP -- show beer list

---
+++ app/src/main/java/beer/brew/vendingmachine/ui/beer/BeerActivity.java
@@ -0,0 +1,15 @@
+package beer.brew.vendingmachine.ui.beer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import beer.brew.vendingmachine.R;
+
+public class BeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beer);
+    }
+}
---
+++ app/src/main/java/beer/brew/vendingmachine/ui/beer/BeerPresenter.java
@@ -0,0 +1,6 @@
+package beer.brew.vendingmachine.ui.beer;
+
+import beer.brew.vendingmachine.ui.base.BasePresenter;
+
+public class BeerPresenter extends BasePresenter<BeerView> {
+}
