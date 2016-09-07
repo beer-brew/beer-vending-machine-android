5b91db77d976421fdb445f71824a4061b1e42317
nancymi <13121178nancy@gmail.com>
2016-09-07 14:30:16 +0800

    Update: beer structure

--- app/src/main/java/beer/brew/vendingmachine/data/model/Beer.java
+++ app/src/main/java/beer/brew/vendingmachine/data/model/Beer.java
@@ -13,10 +13,17 @@ public class Beer {
     @SerializedName("image")
     private final String image;
 
-    public Beer(String name, float price, String image) {
+    private int size;
+
+    public static final int SIZE_SMALL = 0;
+    public static final int SIZE_MIDDLE = 1;
+    public static final int SIZE_BIG = 2;
+
+    public Beer(String name, String image, int size) {
         this.name = name;
-        this.price = price;
         this.image = image;
+        this.size = size;
+        this.setPrice(size);
     }
 
     public String getImage() {
@@ -31,7 +38,16 @@ public class Beer {
         return price;
     }
 
-    public void setPrice(float price) {
-        this.price = price;
+    public int getSize() {
+        return size;
+    }
+
+    private void setPrice(int size) {
+        switch (size) {
+            case SIZE_SMALL: price = (float) 3.0; break;
+            case SIZE_MIDDLE: price = (float) 4.0; break;
+            case SIZE_BIG: price = (float) 5.0; break;
+            default: break;
+        }
     }
 }
