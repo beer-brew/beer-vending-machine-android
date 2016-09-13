96a3fadccc243e1cf59e3ab81b31653ab836a018
nancymi <13121178nancy@gmail.com>
2016-09-13 18:13:41 +0800

    Add: Wechat callback Activity

---
+++ app/src/main/java/net/sourceforge/simcpux/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,54 @@
+package net.sourceforge.simcpux.wxapi;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+
+import com.tencent.mm.sdk.constants.ConstantsAPI;
+import com.tencent.mm.sdk.modelbase.BaseReq;
+import com.tencent.mm.sdk.modelbase.BaseResp;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import beer.brew.vendingmachine.R;
+
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler{
+
+    private static final String TAG = "MicroMsg.SDKSample.WXPayEntryActivity";
+
+    private IWXAPI api;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_result);
+
+        api = WXAPIFactory.createWXAPI(this, "wx2989d1fc9d539313");
+        api.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        if (baseResp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.app_tip);
+            builder.setMessage(getString(R.string.pay_result_callback_msg, String.valueOf(baseResp.errCode)));
+            builder.show();
+        }
+    }
+}
