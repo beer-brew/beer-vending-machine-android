340f3f91e9252e5e43a84c22ea2f353ccd9e912f
nancymi <13121178nancy@gmail.com>
2016-09-13 18:11:06 +0800

    Add: payment processor to deal with ali & wechat payment

--- app/src/main/java/beer/brew/vendingmachine/data/remote/PaymentProcessor.java
+++ app/src/main/java/beer/brew/vendingmachine/data/remote/PaymentProcessor.java
@@ -1,6 +1,15 @@
 package beer.brew.vendingmachine.data.remote;
 
+import android.app.Activity;
+import android.content.Context;
+
 import com.alipay.sdk.app.PayTask;
+import com.tencent.mm.sdk.modelpay.PayReq;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Map;
 
@@ -13,6 +22,12 @@ import static rx.schedulers.Schedulers.io;
 
 public class PaymentProcessor {
 
+    private final Context context;
+
+    public PaymentProcessor(Context context) {
+        this.context = context;
+    }
+
     public Observable<PayStatus> pay(String orderInfo, Order.PayType payType) {
         switch (payType) {
             case ALIPAY: return payByAlipay(orderInfo);
@@ -22,7 +37,7 @@ public class PaymentProcessor {
     }
 
     private Observable<PayStatus> payByAlipay(String orderInfo) {
-        final PayTask payTask = new PayTask(context);
+        final PayTask payTask = new PayTask((Activity) context);
         return Observable.just(orderInfo)
                 .subscribeOn(io())
                 .map(new Func1<String, PayStatus>() {
@@ -37,7 +52,30 @@ public class PaymentProcessor {
 
     private Observable<PayStatus> payByWechatPay(String orderInfo) {
         //TODO finish wechat payment processor
-        return Observable.empty();
+        return Observable.just(orderInfo)
+                .map(new Func1<String, PayStatus>() {
+                    @Override
+                    public PayStatus call(String orderInfo) {
+                        IWXAPI api = WXAPIFactory.createWXAPI(context, "wx2989d1fc9d539313");
+                        try {
+                            JSONObject orderJson = new JSONObject(orderInfo);
+                            PayReq req = new PayReq();
+                            req.appId			= orderJson.getString("appid");
+                            req.partnerId		= "1305176001";
+                            req.prepayId		= orderJson.getString("prepay_id");
+                            req.nonceStr		= orderJson.getString("nonce_str");
+                            req.timeStamp		= "1473748346";
+                            req.packageValue	= "wx20160913143226f9efc946130906833589";
+                            req.sign			= orderJson.getString("sign");
+
+                            api.sendReq(req);
+                            return null;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                });
     }
 
     private PayStatus handleAlipayStatus(Map<String, String> payStatus) {
